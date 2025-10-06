fa2be64075314989dda9ea67dea6816670942ad5
DEEN-42 <debanshughosh685@gmail.com>
2025-10-06 22:35:06 +0530

    contract error fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    minify: 'terser', // use Terser for minification
+    terserOptions: {
+      compress: {
+        drop_console: false, // keep all console logs
+        drop_debugger: false,
+      },
+    },
+  },
+});
